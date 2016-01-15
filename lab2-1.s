bec78516c3b22053499eb8a4597dd553ec4a63f3
peter_georgiev <georgiev2592@gmail.com>
2016-01-15 15:15:09 -0800

    added comments to the code

--- lab2-1.s
+++ lab2-1.s
@@ -1,6 +1,6 @@
 .data 
 	mask: .word 0xF0000000
-	nums: .word 0x0000000A
+	split: .word 0x0000000A
 	count: .word 28
 	input: .word 12345678
 	stge: .space 10
@@ -9,15 +9,15 @@
 .globl main
 
 main:
-	la $a0, input				#load input integer into$a0 from memory
+	la $a0, input              #load input integer into$a0 from memory
 	lw $a0, 0($a0)
-	la $a1, stge				#load hex string storage address into $a1
+	la $a1, stge               #load hex string storage address into $a1
 
-	la $s0, mask				#load mask into $s0
+	la $s0, mask               #load mask into $s0
 	lw $s0, 0($s0)
-	la $s1, count 				#load count into $s1
+	la $s1, count              #load count into $s1
 	lw $s1, 0($s1)		
-	la $s2, nums				#load nums into $s2
+	la $s2, split              #load split into $s2
 	lw $s2, 0($s2)	
 
 	loop:
@@ -25,8 +25,8 @@ main:
 		srl $t1, $t1, $s1
 		addi $s1, $s1, -4       #decrement the count by 4 bits (1 byte)
 		srl $s0, $s0, 4
-		bge $t1, $s2, letter 	#branch to hexup if $t1 contains alphabetical hex value (A-F)
-		b number                #branch to hexdown if $t1 contains numberical hex value (0-9)
+		bge $t1, $s2, letter 	#branch to letter if $t1 contains alphabetical hex value (A-F)
+		b number                #branch to number if $t1 contains numberical hex value (0-9)
 
 	letter:
 		addi $t1, $t1, 55 		#obtain ascii value and store into stge
@@ -43,11 +43,11 @@ main:
 		b loop
 
 	finish:
-      addi $t1, $t1, 1
+      addi $t1, $t1, 1        #add null terminator at the end of the storage area
 		li $t1, 0
 		sb $t1, 0($a1)
 		li $v0, 4               #load up syscalll for printing
-      la $a0, stge
+      la $a0, stge            #print the value from the storage area
       syscall
 
 		li $v0, 10
