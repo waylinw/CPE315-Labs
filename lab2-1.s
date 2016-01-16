6593970119a85158b9b7404c16aece97b2ee767e
waylinw <waylinw@yahoo.com>
2016-01-15 17:10:35 -0800

    changes for 1 and 3
    changed 1 into function instead of subroutines
    copied it over to 3

--- lab2-1.s
+++ lab2-1.s
@@ -3,6 +3,7 @@
 	split: .word 0x0000000A
 	count: .word 28
 	input: .word 12345678
+   test:  .word 99999999
 	stge: .space 10
 .text
 
@@ -10,22 +11,30 @@
 
 main:
    la $s3, input              #load input integer into$a0 from memory
-   lw $s3, 0($s3)             #test comment
+   lw $a0, 0($s3)             #test comment
 
+   jal bintohex
 
-   bintohex:
+   la $a0, test
+   lw $a0, 0($a0)
+   jal bintohex
+
+   li $v0, 10
+   syscall
+
+bintohex:
       #it's up to you to decide from which register you want to load the integer
       #my example uses $s3
 
-      move $a0, $s3              #load input integer into $a0 from memory
-      la $a1, stge               #load hex string storage address into $a1
+     #move $a0, $s3              #load input integer into $a0 from memory
+   la $a1, stge               #load hex string storage address into $a1
 
-      la $s0, mask               #load mask into $s0
-      lw $s0, 0($s0)
-      la $s1, count              #load count into $s1
-      lw $s1, 0($s1)
-      la $s2, split              #load split into $s2
-      lw $s2, 0($s2)
+   la $s0, mask               #load mask into $s0
+   lw $s0, 0($s0)
+   la $s1, count              #load count into $s1
+   lw $s1, 0($s1)
+   la $s2, split              #load split into $s2
+   lw $s2, 0($s2)
 
 	loop:
 		and $t1, $s0, $a0 		#load the result of $t0 and $a0 into $t1
@@ -56,7 +65,5 @@ main:
 		li $v0, 4               #load up syscalll for printing
       la $a0, stge            #print the value from the storage area
       syscall
-
-		li $v0, 10
-		syscall
+jr $ra
 .end
